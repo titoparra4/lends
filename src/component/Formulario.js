cf142d24cccd6942b6d912b047da15e7962cc576
Tito <tito.parra4@@hotmail.com>
2019-02-19 21:34:34 +0100

    Create Formulario.js

---
+++ src/component/Formulario.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Formulario extends Component {
+	state = {};
+	render() {
+		return <p>Hola from Formulary</p>;
+	}
+}
+
+export default Formulario;
